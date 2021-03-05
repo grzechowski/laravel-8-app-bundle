269aa8527ff265d74f83db25e7d4acc09c38ece2
Tomek Grzechowski (yodahack) <keo@becomewater.com>
2021-03-05 16:53:44 +0100

    fixing bootstrap 4 because 5 has build issues

--- routes/spa.php
+++ routes/spa.php
@@ -13,4 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('{path}', Grzechowski\MeritoLvssb\Http\Controllers\SpaController::class)->where('path', '(.*)');
+Route::get('{path}', SpaController::class)->where('path', '(.*)');
